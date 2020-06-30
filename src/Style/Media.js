0ded156447f47346a085aec9d83fe899c58d4f50
angryboo <buyongsong@gmail.com>
2020-06-30 14:15:32 +0900

    add: generate media query function

---
+++ src/Style/Media.js
@@ -0,0 +1,25 @@
+import { css } from 'styled-components';
+
+const sizes = {
+  desktop: 768,
+  tablet: 768,
+  phone: 600,
+};
+export default Object.keys(sizes).reduce((acc, label) => {
+  if (label === 'desktop') {
+    acc[label] = (...args) => css`
+      @media (min-width: ${sizes[label]}px) {
+        ${css(...args)};
+      }
+    `;
+  } else {
+    acc[label] = (...args) => css`
+      @media (max-width: ${sizes[label]}px) {
+        ${css(...args)};
+      }
+    `;
+  }
+
+  console.log(acc);
+  return acc;
+}, {});
