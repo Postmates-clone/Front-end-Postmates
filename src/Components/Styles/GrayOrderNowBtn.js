e70013842ea6525838cb61791fffdd47b9572605
pkt0831 <pkt0831@naver.com>
2020-06-30 16:07:58 +0900

    feat:Add GrayOrderBtn.js

---
+++ src/Components/Styles/GrayOrderNowBtn.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const GrayOrderNow = styled.button`
+  padding: 0;
+  margin: 0;
+  border: none;
+  outline: none;
+  font-family: 'PostmatesStd', Helvetica Neue, Helvetica;
+  font-size: 0.75rem;
+  letter-spacing: 0.72px;
+  font-weight: 600;
+  text-transform: uppercase;
+  line-height: normal;
+  cursor: pointer;
+  text-align: center;
+  height: 3rem;
+  transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
+  border-radius: 28px;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 10.5rem;
+  background: #d9dbe0;
+`;
+
+function GreenOrderNowBtn() {
+  return <GrayOrderNow />;
+}
+export default GreenOrderNowBtn;
--- src/Components/Styles/StyleSample.js
+++ src/Components/Styles/StyleSample.js
@@ -96,29 +96,6 @@ const Postmates = styled.div`
   line-height: normal;
   text-rendering: optimizelegibility;
 `;
-// const GreenOrderNowBtn = styled.button`
-//   padding: 0;
-//   margin: 0;
-//   border: none;
-//   outline: none;
-//   font-family: 'PostmatesStd', Helvetica Neue, Helvetica;
-//   font-size: 0.75rem;
-//   letter-spacing: 0.72px;
-//   font-weight: 600;
-//   text-transform: uppercase;
-//   line-height: normal;
-//   cursor: pointer;
-//   text-align: center;
-//   transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
-//   border-radius: 28px;
-//   background-color: #00cc99;
-//   color: #fff;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   height: 3rem;
-//   width: 10.5rem;
-// `;
 const GrayOrderNowBtn = styled.button`
   padding: 0;
   margin: 0;
