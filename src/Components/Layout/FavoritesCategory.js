e18cced519e629b911e158bb20701a864c592bb8
angryboo <buyongsong@gmail.com>
2020-07-01 21:37:28 +0900

    fix: fix favorite text

--- src/Components/Layout/FavoritesCategory.js
+++ src/Components/Layout/FavoritesCategory.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const FavoritesCategory = () => {
-  return <div>FavoritesCategory</div>;
+  return <div>FavoritesCategory(api 작업 완료 후 추가 예정)</div>;
 };
 
 export default FavoritesCategory;
