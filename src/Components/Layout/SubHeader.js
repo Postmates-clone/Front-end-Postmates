bb0924cea6236654fc09387f93526ad5798e35f6
angryboo <buyongsong@gmail.com>
2020-07-02 20:15:35 +0900

    add: generate sub banner

--- src/Components/Layout/SubHeader.js
+++ src/Components/Layout/SubHeader.js
@@ -1,8 +1,23 @@
 // 0701 seungeun
 import React from 'react';
+import styled from 'styled-components';
+import Media from '../../Style/Media';
+
+const SubHeaderBlock = styled.div`
+  height: 50px;
+  ${Media.desktop`
+  background-color: #0079FA;
+  `}
+  ${Media.tablet`
+  background-color: #A8FA00;
+  `}
+  ${Media.mobile`
+  background-color: #F79700;
+  `}
+`;
 
 const SubHeader = () => {
-  return <div>Sub Header</div>;
+  return <SubHeaderBlock>Sub Header</SubHeaderBlock>;
 };
 
 export default SubHeader;
