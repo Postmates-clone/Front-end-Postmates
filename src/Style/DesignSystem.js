08b052484f89e6d1f74d3e7f4e549703b38ae727
Song Seungeun <grayandjj@gmail.com>
2020-07-02 16:33:44 +0900

    add: design system basic button module, fix: global style

--- src/Style/DesignSystem.js
+++ src/Style/DesignSystem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { BasicBtn, ActiveBasicBtn } from './BasicBtn';
+import { BasicBtn, IconBtn } from './BasicBtn';
 
 const DesignSystemBlock = styled.div`
   padding: 50px;
@@ -13,9 +13,16 @@ const Empty = styled.div`
 const DesignSystem = () => {
   return (
     <DesignSystemBlock>
-      <ActiveBasicBtn text="ORDER NOW" />
+      {/* 활성화 버튼의 경우(녹색배경) active, 비활성화 버튼의 경우(회색배경) active={false} */}
+      {/* 기본 여백과 다른 버튼의 경우 width="000px" */}
+      {/* 미디어쿼리 타블렛 twidth="000px" mwidth="000px" */}
+      <IconBtn active text="GET NOW" icon="android" />
       <Empty />
-      <BasicBtn text="ORDER NOW" />
+      <IconBtn active text="GET NOW" icon="apple" />
+      <Empty />
+      <BasicBtn active text="ORDER NOW" mwidth="265px" />
+      <Empty />
+      <BasicBtn active={false} text="ORDER NOW" twidth="500px" />
     </DesignSystemBlock>
   );
 };
