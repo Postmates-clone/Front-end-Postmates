739fbc675c5778a3e36350541b83cfb800b75991
angryboo <buyongsong@gmail.com>
2020-07-02 11:26:31 +0900

    fix: props to item popup

--- src/Components/Pages/Item.js
+++ src/Components/Pages/Item.js
@@ -1,11 +1,9 @@
 /* eslint-disable react/no-children-prop */
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import StoreIndividual from '../Layout/StoreIndividual';
 import MenuList from '../Layout/MenuList';
-// FIXME: 희진 -itemPopup 작업
-import ItemPopup from '../Items/ItemPopup';
 
 const StorePageBlock = styled.main`
   width: 100%;
@@ -15,31 +13,10 @@ const StorePageBlock = styled.main`
 `;
 
 const ItemPage = ({ storeData }) => {
-  const [dialog, setDialog] = useState(false);
-
-  const onClick = () => {
-    setDialog(true);
-  };
-
-  const onCancel = () => {
-    setDialog(false);
-  };
-
   return (
     <StorePageBlock>
       <StoreIndividual storeData={storeData} />
       <MenuList storeData={storeData} />
-      {/* 희진-아이템 팝업영역 */}
-      <button type="button" onClick={onClick}>
-        희진팝업테스트버튼
-      </button>
-      <ItemPopup
-        title="test"
-        children="testtesttest"
-        buttonText="ADD TO CART"
-        visible={dialog}
-        onCancel={onCancel}
-      />
     </StorePageBlock>
   );
 };
