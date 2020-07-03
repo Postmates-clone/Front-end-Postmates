7ac2df29319549212ef93c45fab6e3e7ff7d41b8
Heejin Lee <heejinlee07@gmail.com>
2020-07-03 22:02:35 +0900

    add: basicbtn

--- src/Components/Items/Cart.js
+++ src/Components/Items/Cart.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { BasicBtn } from '../../Style/BasicBtn';
 
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <>
+      <div>Cart</div>;
+      <BasicBtn />
+      BasicBtn
+    </>
+  );
 };
 
 export default Cart;
