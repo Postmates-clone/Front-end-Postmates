ab887c1d466ace0459ddabdf4305d6adc329c3c8
angryboo <buyongsong@gmail.com>
2020-07-03 14:39:45 +0900

    merge

--- src/Components/Items/SubLocation.js
+++ src/Components/Items/SubLocation.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 // import Media from '../../Style/Media';
-import Icon from '../../Style/IconStyles';
+import { location } from '../../Style/IconStyles';
 
 const SubLocationBlock = styled.div`
   background-color: yellow;
 `;
 
 const SubLocation = () => {
-  return <SubLocationBlock>{Icon.location}</SubLocationBlock>;
+  return <SubLocationBlock>{location}</SubLocationBlock>;
 };
 
 export default SubLocation;
--- src/Style/IconStyles.js
+++ src/Style/IconStyles.js
@@ -1,20 +1,25 @@
 import React from 'react';
 
-const icon = {
-  location: (
-    <svg width="14" height="17">
-      <g fill="none" fillRule="evenodd">
-        <path
-          d="M7 10a3 3 0 110-6 3 3 0 010 6zm0-1a2 2 0 100-4 2 2 0 000 4z"
-          fill="#8F95A3"
-        />
-        <path
-          d="M7 16.362c4.35-3.483 6.5-6.616 6.5-9.362C13.5 3.324 10.638.5 7 .5A6.5 6.5 0 00.5 7c0 2.746 2.15 5.879 6.5 9.362z"
-          stroke="#8F95A3"
-        />
-      </g>
-    </svg>
-  ),
-};
+export const location = (
+  <svg width="14" height="17">
+    <g fill="none" fillRule="evenodd">
+      <path
+        d="M7 10a3 3 0 110-6 3 3 0 010 6zm0-1a2 2 0 100-4 2 2 0 000 4z"
+        fill="#8F95A3"
+      />
+      <path
+        d="M7 16.362c4.35-3.483 6.5-6.616 6.5-9.362C13.5 3.324 10.638.5 7 .5A6.5 6.5 0 00.5 7c0 2.746 2.15 5.879 6.5 9.362z"
+        stroke="#8F95A3"
+      />
+    </g>
+  </svg>
+);
 
-export default icon;
+export const check = (
+  <svg width="14" height="14" viewBox="0 0 14 14">
+    <g fill="none" fillRule="evenodd">
+      <rect fill="#0C9" width="14" height="14" rx="7" />
+      <path fill="#FFF" d="M3 7l3 3 4.5-4.5-1-1L6 8 4 6" />
+    </g>
+  </svg>
+);
