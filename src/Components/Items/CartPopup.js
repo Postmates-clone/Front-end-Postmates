a3c9cda18a78002598384215c3b1a5677f8af7b9
Heejin Lee <heejinlee07@gmail.com>
2020-07-08 01:45:50 +0900

    fix: cart style issue solved

--- src/Components/Items/CartPopup.js
+++ src/Components/Items/CartPopup.js
@@ -11,10 +11,13 @@
 import React, { useState, useEffect } from 'react';
 import styled, { keyframes, css } from 'styled-components';
 import { useSelector, useDispatch } from 'react-redux';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css'; // optional
 import { Button } from './ProductInfo';
 import { REMOVE_FROM_CART } from '../../Modules/CartReducer';
-import { IconImage } from '../Layout/MainBanner';
-import xIcon from '../../Assets/xicon.png';
+import { CloseBtn } from '../../Style/PopUp';
+import { closeIcon } from '../../Style/IconStyles';
+import { itemColor } from './ItemPopup';
 
 const fadeIn = keyframes`
 from{
@@ -55,9 +58,13 @@ to{
 const DialogBlock = styled.div`
   width: 350px;
   height: 350px;
-  padding-left: 1.5rem;
+  padding: 1.5rem;
   background: #fff;
   position: absolute;
+  overflow-y: hidden;
+  right: 0;
+  margin-top: 10px;
+  border: 1px solid lightgray;
 
   h1 {
     margin: 0;
@@ -85,13 +92,48 @@ const DialogBlock = styled.div`
     `}
 `;
 
+const ContentBlock = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+  margin: 10px 0;
+  width: 100%;
+`;
+
+const CountBlock = styled.div`
+  min-width: 10%;
+  text-align: left;
+`;
+
+const DetailBlock = styled.div`
+  min-width: 60%;
+  display: flex;
+  flex-direction: column;
+`;
+const DetailNameBlock = styled.div``;
+
+const DetailOptionBlock = styled.div``;
+
+const PriceBlock = styled.div`
+  min-width: 20%;
+`;
+
+const RemoveBlock = styled.div`
+  min-width: 10%;
+  text-align: right;
+`;
+
+const RemoveBtn = styled.div`
+  cursor: pointer;
+`;
+
 const ButtonGroup = styled.div`
   margin-top: 3rem;
   display: flex;
   justify-content: center;
 `;
 
-const CartPopup = ({ item, visible, onCancel }) => {
+const CartPopup = ({ item, visible }) => {
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.Cart.cart);
   const [animate, setAnimate] = useState(false);
@@ -113,39 +155,40 @@ const CartPopup = ({ item, visible, onCancel }) => {
   }, [localVisible, visible]);
 
   if (!animate && !localVisible) return null;
+
   return (
     <>
       {!onDialog && (
         <DialogBlock disappear={!visible}>
-          {cart.map((item) => (
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                margin: '10px 0',
-              }}
-            >
-              <span>{item.name}</span>
-              <span>{item.count}</span>
-              <span>
-                {`$${Number(item.base_price * item.count).toFixed(2)}`}
-              </span>
-              <span>선택한 옵션</span>
-              <span>{item.instruction}</span>
-              <IconImage
-                cursor
-                src={xIcon}
-                onClick={() => onRemove(item.name)}
-              />
-              <hr />
-            </div>
-          ))}
+          <h2>Order</h2>
+          <ContentBlock>
+            {cart.map((item) => (
+              <>
+                <CountBlock>{item.count}</CountBlock>
+                <DetailBlock>
+                  <DetailNameBlock>{item.name}</DetailNameBlock>
+                  <DetailOptionBlock>선택한 옵션</DetailOptionBlock>
+                  <span>{item.instruction}</span>
+                  <span>
+                    {`$${Number(item.base_price * item.count).toFixed(2)}`}
+                  </span>
+                </DetailBlock>
+                <RemoveBlock>
+                  <RemoveBtn onClick={() => onRemove(item.name)}>
+                    {closeIcon}
+                  </RemoveBtn>
+                </RemoveBlock>
+              </>
+            ))}
+          </ContentBlock>
           <div>Subtotal</div>
-          {`$${cart
-            .reduce((prev, curr) => {
-              return prev + curr.base_price * curr.count;
-            }, 0)
-            .toFixed(2)}`}
+          <PriceBlock>
+            {`$${cart
+              .reduce((prev, curr) => {
+                return prev + curr.base_price * curr.count;
+              }, 0)
+              .toFixed(2)}`}
+          </PriceBlock>
           <ButtonGroup>
             <Button>CHECKOUT</Button>
           </ButtonGroup>
