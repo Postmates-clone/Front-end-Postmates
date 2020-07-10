7f6d91e0e235fb1198364c732f98a98c8b51ee26
Song Seungeun <grayandjj@gmail.com>
2020-07-10 10:51:17 +0900

    add: order history page

---
+++ src/Components/Pages/OrderHistory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserHeader from '../Items/UserHeader';
+
+const OrderHistory = () => {
+  return (
+    <div>
+      <UserHeader title="Orders" />
+    </div>
+  );
+};
+
+export default OrderHistory;
